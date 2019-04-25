5f7d15ec333d8b837daf506d95f8fb817e00267a
Max Willsey <me@mwillsey.com>
2019-04-25 09:53:32 -0700

    Get simple pattern matching to work

--- src/dot.rs
+++ src/dot.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use crate::{EGraph, Id};
+use crate::{EGraph};
 
 pub struct Dot<'a> {
     egraph: &'a EGraph,
@@ -22,10 +22,9 @@ impl<'a> fmt::Display for Dot<'a> {
         // define all the nodes, clustered by eclass
         for (leader, class) in &self.egraph.classes {
             write!(f, "  subgraph cluster_{} {{\n", leader.0)?;
-            write!(f, "  style=dotted")?;
-            for id in class {
-                let node = &self.egraph.nodes[id.0];
-                write!(f, "    {}[label = \"", id.0)?;
+            write!(f, "    style=dotted")?;
+            for (i, node) in class.iter().enumerate() {
+                write!(f, "    {}.{}[label = \"", leader.0, i)?;
                 node.write_label(f)?;
                 write!(f, "\"]\n")?;
             }
@@ -33,18 +32,21 @@ impl<'a> fmt::Display for Dot<'a> {
         }
 
         let mut children = Vec::new();
-        for (i, node) in self.egraph.nodes.iter().enumerate() {
-            node.fill_children(&mut children);
-            for child in &children {
-                // write the edge to the child, but clip it to the eclass with lhead
-                let child_leader = self.egraph.leaders[child.0];
-                write!(
-                    f,
-                    "  {} -> {} [lhead = cluster_{}]\n",
-                    i, child.0, child_leader.0
-                )?;
+        for (leader, class) in &self.egraph.classes {
+            for (i, node) in class.iter().enumerate() {
+                node.fill_children(&mut children);
+                for child in &children {
+                    // write the edge to the child, but clip it to the eclass with lhead
+                    let child_leader = self.egraph.leaders[child.0];
+                    write!(
+                        f,
+                        // {}.0 to pick an arbitrary node in the cluster
+                        "  {}.{} -> {}.0 [lhead = cluster_{}]\n",
+                        leader.0, i, child.0, child_leader.0
+                    )?;
+                }
+                children.clear();
             }
-            children.clear();
         }
 
         write!(f, "}}")
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 mod dot;
+mod parse;
 
 use std::collections::HashMap;
+
+use log::{info, trace};
 
 /// EClass Id
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Id(usize);
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum ENode {
     Var(String),
     Const(i32),
@@ -22,6 +26,16 @@ impl ENode {
             ENode::Times(..) => write!(f, "*"),
         }
     }
+
+    fn map_ids(&self, mut f: impl FnMut(Id) -> Id) -> ENode {
+        match self {
+            ENode::Var(_) => self.clone(),
+            ENode::Const(_) => self.clone(),
+            ENode::Plus(l, r) => ENode::Plus(f(*l), f(*r)),
+            ENode::Times(l, r) => ENode::Times(f(*l), f(*r)),
+        }
+    }
+
     fn fill_children(&self, vec: &mut Vec<Id>) {
         assert_eq!(vec.len(), 0);
         match self {
@@ -39,11 +53,18 @@ impl ENode {
     }
 }
 
-#[derive(Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct EGraph {
-    nodes: Vec<ENode>,
+    nodes: HashMap<ENode, Id>,
     leaders: Vec<Id>,
-    classes: HashMap<Id, Vec<Id>>,
+    classes: HashMap<Id, Vec<ENode>>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Pattern {
+    nodes: Vec<ENode>,
+    lhs: Id,
+    rhs: Id,
 }
 
 impl EGraph {
@@ -63,6 +84,11 @@ impl EGraph {
         // make sure that total size of classes == all nodes
         let sum_classes = self.classes.values().map(|c| c.len()).sum();
         assert_eq!(self.nodes.len(), sum_classes);
+
+        // make sure that all values of the union find are leaders
+        for leader in &self.leaders {
+            assert_eq!(leader, &self.leaders[leader.0]);
+        }
     }
 
     pub fn len(&self) -> usize {
@@ -72,33 +98,150 @@ impl EGraph {
     pub fn add(&mut self, enode: ENode) -> Id {
         self.check();
 
+        trace!("Adding       {:?}", enode);
+
         // make sure that the enodes children are already in the set
         if cfg!(debug_assertions) {
             let mut ids = Vec::new();
             enode.fill_children(&mut ids);
-            for Id(i) in ids {
-                assert!(i < self.len());
+            for id in ids {
+                assert!(id.0 < self.len());
             }
         }
 
-        let next_id = Id(self.len());
-        self.nodes.push(enode);
-        self.leaders.push(next_id);
-        self.classes.insert(next_id, vec![next_id]);
+        // hash cons
+        let id = match self.nodes.get(&enode) {
+            None => {
+                let next_id = Id(self.len());
+                trace!("Added  {:4}: {:?}", next_id.0, enode);
+                self.leaders.push(next_id);
+                self.classes.insert(next_id, vec![enode.clone()]);
+                self.nodes.insert(enode, next_id);
+                next_id
+            }
+            Some(id) => {
+                trace!("Added *{:4}: {:?}", id.0, enode);
+                *id
+            }
+        };
 
         self.check();
+        id
+    }
 
-        next_id
+    // pub fn pattern_match(&mut self, pattern: &Pattern) -> EGraph {
+    //     // stores (PatternId, EClassId) matches
+    //     let mut database = HashSet::new();
+
+    //     for (leader, class) in self.classes.iter() {
+    //         let mut stack = Vec::new();
+    //         stack.push((start, node));
+
+    //         while let Some((p, n)) = stack.pop() {
+    //             use ENode::*;
+    //             match (p, n) {
+    //                 (Var(s), _) => (),
+    //                 (Plus(pl, pr), Plus(nl, nr)) => {
+    //                     stack.push((pl, nl));
+    //                     stack.push((pr, nr));
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    fn get_eclass(&self, eclass_id: Id) -> &[ENode] {
+        self.classes
+            .get(&eclass_id)
+            .unwrap_or_else(|| panic!("Couldn't find eclass {:?}", eclass_id))
+    }
+
+    fn match_node_against_eclass(
+        &self,
+        mut var_mapping: HashMap<String, Id>,
+        pattern: &Pattern,
+        eclass: Id,
+        pattern_node: ENode,
+    ) -> Vec<HashMap<String, Id>> {
+        if let ENode::Var(s) = pattern_node {
+            match var_mapping.get(&s) {
+                None => {
+                    var_mapping.insert(s, eclass);
+                }
+                Some(&prev_mapped_eclass) => {
+                    if eclass != prev_mapped_eclass {
+                        return vec![];
+                    }
+                }
+            }
+
+            return vec![var_mapping];
+        }
+
+        let mut new_mappings = Vec::new();
+
+        for class_node in self.get_eclass(eclass) {
+            use ENode::*;
+            match (&pattern_node, class_node) {
+                (Var(_), _) => panic!("pattern isn't a var at this point"),
+                (Plus(pl, pr), Plus(nl, nr)) => {
+                    let left_mappings = self.match_node_against_eclass(
+                        var_mapping.clone(),
+                        pattern,
+                        *nl,
+                        pattern.nodes[pl.0].clone(),
+                    );
+
+                    for vm in left_mappings {
+                        new_mappings.extend(self.match_node_against_eclass(
+                            vm,
+                            pattern,
+                            *nr,
+                            pattern.nodes[pr.0].clone(),
+                        ))
+                    }
+                }
+                (Times(_pl, _pr), Times(_nl, _nr)) => unimplemented!(),
+                _ => (),
+            }
+        }
+
+        new_mappings
+    }
+
+    fn rebuild(&mut self) {
+        // TODO don't copy so much
+        let mut new_classes = HashMap::new();
+
+        for (leader, class) in self.classes.iter() {
+            let new_nodes: Vec<_> = class
+                .iter()
+                .map(|node| node.map_ids(|id| self.leaders[id.0]))
+                .collect();
+
+            new_classes.insert(*leader, new_nodes);
+        }
+        self.classes = new_classes;
+
+        self.nodes.clear();
+        for (leader, class) in &self.classes {
+            for node in class {
+                self.nodes.insert(node.clone(), *leader);
+            }
+        }
     }
 
     fn union(&mut self, id1: Id, id2: Id) -> Id {
         self.check();
+
+        trace!("Unioning {} and {}", id1.0, id2.0);
 
         let mut leader1 = self.leaders[id1.0];
         let mut leader2 = self.leaders[id2.0];
 
         // already unioned
         if leader1 == leader2 {
+            trace!("Already unioned...");
             return leader1;
         }
 
@@ -111,20 +254,35 @@ impl EGraph {
             }
         }
 
-        // remove the bigger class, merging into the smaller class
+        // remove the smaller class, merging into the bigger class
         let smaller_class = self.classes.remove(&leader1).unwrap();
-        let bigger_class = self.classes.get_mut(&leader2).unwrap();
+        let bigger_class = self.classes.remove(&leader2).unwrap();
 
-        bigger_class.reserve(smaller_class.len());
-        for id in smaller_class {
-            // TODO transform the nodes that are merged
-            self.leaders[id.0] = leader2;
-            bigger_class.push(id)
+        let mut new_nodes = Vec::with_capacity(smaller_class.len() + bigger_class.len());
+        for node in smaller_class.into_iter().chain(bigger_class) {
+            let old_leader = self.nodes[&node];
+            self.leaders[old_leader.0] = leader2;
+            // let new_node = node.map_ids(|id| self.leaders[id.0]);
+            // new_nodes.push(new_node);
+            new_nodes.push(node);
+            trace!("{:?}", new_nodes);
         }
 
+        self.classes.insert(leader2, new_nodes);
+
         self.check();
+        trace!("Unioned {} -> {}", leader1.0, leader2.0);
+        trace!("Leaders: {:?}", self.leaders);
+        for (leader, class) in &self.classes {
+            trace!("  {:?}: {:?}", leader, class);
+        }
         leader2
     }
+}
+
+#[cfg(test)]
+fn init_logger() {
+    let _ = env_logger::builder().is_test(true).try_init();
 }
 
 #[cfg(test)]
@@ -143,22 +301,76 @@ mod tests {
         let dot = dot::Dot::new(&egraph);
         let mut file = File::create(format!("{}.dot", name)).unwrap();
         write!(file, "{}", dot).unwrap();
-        println!("{}", dot);
+        info!("Writing {}.dot ...\n{}", name, dot);
     }
 
     #[test]
     fn simple_add() {
+        crate::init_logger();
         let mut egraph = EGraph::default();
 
         let x = egraph.add(var("x"));
-        let plus = egraph.add(ENode::Plus(x, x));
+        let x2 = egraph.add(var("x"));
+        let plus = egraph.add(ENode::Plus(x, x2));
 
         let y = egraph.add(var("y"));
 
         egraph.union(x, y);
+        egraph.rebuild();
 
         dot(&egraph, "foo");
 
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn simple_match() {
+        crate::init_logger();
+        let mut egraph = EGraph::default();
+
+        let x = egraph.add(var("x"));
+        let y = egraph.add(var("y"));
+        let plus = egraph.add(ENode::Plus(x, y));
+
+        let z = egraph.add(var("z"));
+        let w = egraph.add(var("w"));
+        let plus2 = egraph.add(ENode::Plus(z, w));
+
+        egraph.union(plus, plus2);
+        egraph.rebuild();
+
+        let commute_plus = Pattern {
+            nodes: vec![
+                var("a"),
+                var("b"),
+                ENode::Plus(Id(0), Id(1)),
+                ENode::Plus(Id(1), Id(0)),
+            ],
+            lhs: Id(2),
+            rhs: Id(3),
+        };
+
+        let mappings = egraph.match_node_against_eclass(
+            HashMap::new(),
+            &commute_plus,
+            egraph.leaders[plus.0],
+            commute_plus.nodes[commute_plus.lhs.0].clone(),
+        );
+
+        let mut expected1 = HashMap::new();
+        expected1.insert("a".into(), x);
+        expected1.insert("b".into(), y);
+
+        let mut expected2 = HashMap::new();
+        expected2.insert("a".into(), z);
+        expected2.insert("b".into(), w);
+
+        let expected_mappings = vec![expected1, expected2];
+        assert_eq!(expected_mappings, mappings);
+
+        info!("Here are the mappings!");
+        for m in mappings {
+            info!("mappings: {:?}", m);
+        }
     }
 }
