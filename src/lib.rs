d22c80a9d6052e1f36bcb3cfb4b8994b983786be
Max Willsey <me@mwillsey.com>
2020-07-14 15:39:26 -0700

    Get started with a tutorial

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,24 @@
 #![warn(missing_docs)]
 /*!
-[`EGraph`]s (and almost everything else in this crate) are
-parameterized over the language given by the user (by implementing
-the [`Language`] trait).
 
-If your Language implements [`FromStr`] (and Languages derived using
-[`define_language!`] do), you can easily create [`RecExpr`]s to add to
-an [`EGraph`].
+`egg` is a e-graph library optimized for equality saturation.
 
-[`EGraph`]: struct.EGraph.html
-[`Language`]: trait.Language.html
-[`RecExpr`]: struct.RecExpr.html
-[`define_language!`]: macro.define_language.html
-[`FromStr`]: https://doc.rust-lang.org/std/str/trait.FromStr.html
+This is the API documentation.
 
-Add `egg` to your `Cargo.toml` like this:
-```toml
-[dependencies]
-egg = "0.5.0"
-```
+The [tutorial](tutorials/index.html) is a good starting point if you're new to
+e-graphs, equality saturation, or Rust.
 
-# Example
+The [tests](https://github.com/mwillsey/egg/tree/master/tests)
+on Github provide some more elaborate examples.
 
-```
-use egg::{*, rewrite as rw};
+There is also a [paper](https://arxiv.org/abs/2004.03082)
+describing `egg` and some of its technical novelties.
 
-define_language! {
-    enum SimpleLanguage {
-        Num(i32),
-        "+" = Add([Id; 2]),
-        "*" = Mul([Id; 2]),
-        // language items are parsed in order, and we want symbol to
-        // be a fallback, so we put it last.
-        // `Symbol` is an egg-provided interned string type
-        Symbol(egg::Symbol),
-    }
-}
-
-let rules: &[Rewrite<SimpleLanguage, ()>] = &[
-    rw!("commute-add"; "(+ ?a ?b)" => "(+ ?b ?a)"),
-    rw!("commute-mul"; "(* ?a ?b)" => "(* ?b ?a)"),
-
-    rw!("add-0"; "(+ ?a 0)" => "?a"),
-    rw!("mul-0"; "(* ?a 0)" => "0"),
-    rw!("mul-1"; "(* ?a 1)" => "?a"),
-];
-
-let start = "(+ 0 (* 1 foo))".parse().unwrap();
-let runner = Runner::default().with_expr(&start).run(rules);
-println!(
-    "Stopped after {} iterations, reason: {:?}",
-    runner.iterations.len(),
-    runner.stop_reason
-);
-```
 !*/
 
 mod macros;
+
+pub mod tutorials;
 
 mod dot;
 mod eclass;
