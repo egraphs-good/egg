3f8ff7777e09a8984da30b2091ff9716fd06a703
Max Willsey <me@mwillsey.com>
2020-10-27 10:56:36 -0700

    Format

--- src/run.rs
+++ src/run.rs
@@ -280,7 +280,7 @@ where
 
     /// Add a hook to instrument or modify the behavior of a [`Runner`].
     /// Each hook will run at the beginning of each iteration, i.e. before
-    /// all the rewrites. 
+    /// all the rewrites.
     ///
     /// If your hook modifies the e-graph, make sure to call
     /// [`rebuild`](struct.EGraph.html#method.rebuild).
