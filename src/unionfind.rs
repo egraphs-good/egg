5d12e81a43d8eb6bfa8c29b62ba766143ba54a79
Max Willsey <me@mwillsey.com>
2021-02-22 15:29:45 -0800

    Simplify unionfind

--- src/unionfind.rs
+++ src/unionfind.rs
@@ -1,7 +1,6 @@
 use crate::Id;
 use std::fmt::Debug;
 
-
 #[derive(Debug, Clone, Default)]
 pub struct UnionFind {
     parents: Vec<Id>,
@@ -14,134 +13,78 @@ impl UnionFind {
         id
     }
 
-    fn is_root(&self, query: Id) -> bool {
-        query == self.parents[usize::from(query)]
+    fn parent(&self, query: Id) -> Id {
+        self.parents[usize::from(query)]
     }
 
-    // we use unsafe slice accesses internally, but all public Ids
-    // coming in are checked first
-    #[inline(always)]
-    unsafe fn parent(&self, query: Id) -> Id {
-        *self.parents.get_unchecked(usize::from(query))
-    }
-
-    #[inline(always)]
-    unsafe fn parent_mut(&mut self, query: Id) -> &mut Id {
-        self.parents.get_unchecked_mut(usize::from(query))
+    fn parent_mut(&mut self, query: Id) -> &mut Id {
+        &mut self.parents[usize::from(query)]
     }
 
     pub fn find(&self, mut current: Id) -> Id {
-        assert!(usize::from(current) < self.parents.len());
-        unsafe {
-            while current != self.parent(current) {
-                current = self.parent(current)
-            }
+        while current != self.parent(current) {
+            current = self.parent(current)
         }
         current
     }
 
     pub fn find_mut(&mut self, mut current: Id) -> Id {
-        assert!(usize::from(current) < self.parents.len());
-        unsafe {
-            while current != self.parent(current) {
-                let grandparent = self.parent(self.parent(current));
-                *self.parent_mut(current) = grandparent;
-                current = grandparent;
-            }
+        while current != self.parent(current) {
+            let grandparent = self.parent(self.parent(current));
+            *self.parent_mut(current) = grandparent;
+            current = grandparent;
         }
         current
     }
 
     /// Returns (new_leader, old_leader)
-    pub fn union(&mut self, root1: Id, root2: Id) -> (Id, Id) {
-        assert!(self.is_root(root1));
-        assert!(self.is_root(root2));
+    pub fn union(&mut self, root1: Id, root2: Id) -> Id {
+        assert_eq!(root1, self.parent(root1));
+        assert_eq!(root2, self.parent(root2));
         assert_ne!(root1, root2);
-
-        unsafe {
-            *self.parent_mut(root2) = root1;
-        }
-        (root1, root2)
+        *self.parent_mut(root2) = root1;
+        root1
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     use indexmap::{indexmap, indexset, IndexMap, IndexSet};
+    fn ids(us: impl IntoIterator<Item = usize>) -> Vec<Id> {
+        us.into_iter().map(|u| u.into()).collect()
+    }
 
-//     impl UnionFind {
-//         pub fn build_sets(&mut self) -> IndexMap<Id, IndexSet<Id>> {
-//             let mut map: IndexMap<Id, IndexSet<Id>> = Default::default();
+    #[test]
+    fn union_find() {
+        let n = 10;
+        let id = |u: usize| Id::from(u);
 
-//             for i in 0..self.parents.len() {
-//                 let i = Id::from(i);
-//                 let leader = self.find_mut(i);
-//                 map.entry(leader).or_default().insert(i);
-//             }
+        let mut uf = UnionFind::default();
+        for _ in 0..n {
+            uf.make_set();
+        }
 
-//             map
-//         }
-//     }
+        // test the initial condition of everyone in their own set
+        assert_eq!(uf.parents, ids(0..n));
 
-//     fn make_union_find(n: usize) -> UnionFind {
-//         let mut uf = UnionFind::default();
-//         for _ in 0..n {
-//             uf.make_set();
-//         }
-//         uf
-//     }
+        // build up one set
+        uf.union(id(0), id(1));
+        uf.union(id(0), id(2));
+        uf.union(id(0), id(3));
 
-//     #[test]
-//     fn union_find() {
-//         let n = 10;
+        // build up another set
+        uf.union(id(6), id(7));
+        uf.union(id(6), id(8));
+        uf.union(id(6), id(9));
 
-//         fn id(u: usize) -> Id {
-//             u.into()
-//         }
+        // this should compress all paths
+        for i in 0..n {
+            uf.find_mut(id(i));
+        }
 
-//         let mut uf = make_union_find(n);
-
-//         // test the initial condition of everyone in their own set
-//         for i in 0..n {
-//             let i = Id::from(i);
-//             assert_eq!(uf.find_mut(i), i);
-//             assert_eq!(uf.find_mut(i), i);
-//         }
-
-//         // make sure build_sets works
-//         let expected_sets = (0..n)
-//             .map(|i| (id(i), indexset!(id(i))))
-//             .collect::<IndexMap<_, _>>();
-//         assert_eq!(uf.build_sets(), expected_sets);
-
-//         // build up one set
-//         assert_eq!(uf.union(id(0), id(1)), (id(0), id(1)));
-//         assert_eq!(uf.union(id(1), id(2)), (id(0), id(2)));
-//         assert_eq!(uf.union(id(3), id(2)), (id(0), id(3)));
-
-//         // build up another set
-//         assert_eq!(uf.union(id(6), id(7)), (id(6), id(7)));
-//         assert_eq!(uf.union(id(8), id(9)), (id(8), id(9)));
-//         assert_eq!(uf.union(id(7), id(9)), (id(6), id(8)));
-
-//         // make sure union on same set returns to == from
-//         assert_eq!(uf.union(id(1), id(3)), (id(0), id(0)));
-//         assert_eq!(uf.union(id(7), id(8)), (id(6), id(6)));
-
-//         // check set structure
-//         let expected_sets = indexmap!(
-//             id(0) => indexset!(id(0), id(1), id(2), id(3)),
-//             id(4) => indexset!(id(4)),
-//             id(5) => indexset!(id(5)),
-//             id(6) => indexset!(id(6), id(7), id(8), id(9)),
-//         );
-//         assert_eq!(uf.build_sets(), expected_sets);
-
-//         // all paths should be compressed at this point
-//         for i in 0..n {
-//             assert_eq!(uf.parent(id(i)), uf.find_mut(id(i)));
-//         }
-//     }
-// }
+        // indexes:         0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        let expected = vec![0, 0, 0, 0, 4, 5, 6, 6, 6, 6];
+        assert_eq!(uf.parents, ids(expected));
+    }
+}
