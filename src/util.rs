061f92c22d24415b7b92e669ccaa90640ede4326
Heres, Daniel <danielheres@gmail.com>
2021-03-27 16:42:34 +0100

    Fmt

--- src/util.rs
+++ src/util.rs
@@ -9,8 +9,7 @@ use crate::*;
 
 pub(crate) type BuildHasher = fxhash::FxBuildHasher;
 
-pub(crate) type HashMap<K, V> =
-    hashbrown::HashMap<K, V, BuildHasher>;
+pub(crate) type HashMap<K, V> = hashbrown::HashMap<K, V, BuildHasher>;
 pub(crate) type HashSet<K> = hashbrown::HashSet<K, BuildHasher>;
 
 pub(crate) type IndexMap<K, V> = indexmap::IndexMap<K, V, BuildHasher>;
