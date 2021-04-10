8d50af78685f5a3e02b662a3f35ef881d5660883
mwillsey <me@mwillsey.com>
2021-04-10 23:58:13 +0000

    Deploying to gh-pages from  @ 4b14701c2324191c15049b375df7a56170fb1a37 🚀

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["egg"] = [{"text":"impl&lt;L&gt; AsRef&lt;[L]&gt; for RecExpr&lt;L&gt;","synthetic":false,"types":[]}];
+implementors["egg"] = [{"text":"impl&lt;L&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">[</a>L<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">]</a>&gt; for <a class=\"struct\" href=\"egg/struct.RecExpr.html\" title=\"struct egg::RecExpr\">RecExpr</a>&lt;L&gt;","synthetic":false,"types":["egg::language::RecExpr"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
