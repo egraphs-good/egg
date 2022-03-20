686803792e50d9034086ef39c7065d85653e4f7c
Chad Brewbaker <crb002@Chads-MacBook-Air.local>
2022-03-20 15:12:23 -0500

    added references and basic description

---
+++ tests/finset.rs
@@ -0,0 +1,167 @@
+use egg::*;
+
+// http://chadbrewbaker.github.io/algebra/haskell/functional/programming/2016/01/27/algebraRules.html
+// Emily Reihl, Category Theory in Context example 1.4.9 
+
+define_language! {
+    enum Prop {
+        Bool(bool),           // sets A, B
+        "^" = Exp([Id; 2]), // set of functions A -> B  , denoted B^A
+        "+" = Or([Id; 2]),  // disjoint union of A, B 
+        "*" = And([Id; 2]), // Cartesian product of A,B
+        Symbol(Symbol),
+    }
+}
+
+type EGraph = egg::EGraph<Prop, ConstantFold>;
+type Rewrite = egg::Rewrite<Prop, ConstantFold>;
+
+#[derive(Default)]
+struct ConstantFold;
+impl Analysis<Prop> for ConstantFold {
+    type Data = Option<(bool, PatternAst<Prop>)>;
+    fn merge(&mut self, to: &mut Self::Data, from: Self::Data) -> DidMerge {
+        merge_max(to, from)
+    }
+
+    fn make(egraph: &EGraph, enode: &Prop) -> Self::Data {
+        let x = |i: &Id| egraph[*i].data.as_ref().map(|c| c.0);
+        let result = match enode {
+            Prop::Bool(c) => Some((*c, c.to_string().parse().unwrap())),
+            Prop::Symbol(_) => None,
+            Prop::And([a, b]) => Some((
+                x(a)? && x(b)?,
+                format!("(& {} {})", x(a)?, x(b)?).parse().unwrap(),
+            )),
+            Prop::Not(a) => Some((!x(a)?, format!("(~ {})", x(a)?).parse().unwrap())),
+            Prop::Or([a, b]) => Some((
+                x(a)? || x(b)?,
+                format!("(| {} {})", x(a)?, x(b)?).parse().unwrap(),
+            )),
+            Prop::Implies([a, b]) => Some((
+                x(a)? || !x(b)?,
+                format!("(-> {} {})", x(a)?, x(b)?).parse().unwrap(),
+            )),
+        };
+        println!("Make: {:?} -> {:?}", enode, result);
+        result
+    }
+
+    fn modify(egraph: &mut EGraph, id: Id) {
+        if let Some(c) = egraph[id].data.clone() {
+            egraph.union_instantiations(
+                &c.1,
+                &c.0.to_string().parse().unwrap(),
+                &Default::default(),
+                "analysis".to_string(),
+            );
+        }
+    }
+}
+
+macro_rules! rule {
+    ($name:ident, $left:literal, $right:literal) => {
+        #[allow(dead_code)]
+        fn $name() -> Rewrite {
+            rewrite!(stringify!($name); $left => $right)
+        }
+    };
+    ($name:ident, $name2:ident, $left:literal, $right:literal) => {
+        rule!($name, $left, $right);
+        rule!($name2, $right, $left);
+    };
+}
+
+rule! {def_imply, def_imply_flip,   "(-> ?a ?b)",       "(| (~ ?a) ?b)"          }
+rule! {double_neg, double_neg_flip,  "(~ (~ ?a))",       "?a"                     }
+rule! {assoc_or,    "(| ?a (| ?b ?c))", "(| (| ?a ?b) ?c)"       }
+rule! {dist_and_or, "(& ?a (| ?b ?c))", "(| (& ?a ?b) (& ?a ?c))"}
+rule! {dist_or_and, "(| ?a (& ?b ?c))", "(& (| ?a ?b) (| ?a ?c))"}
+rule! {comm_or,     "(| ?a ?b)",        "(| ?b ?a)"              }
+rule! {comm_and,    "(& ?a ?b)",        "(& ?b ?a)"              }
+rule! {lem,         "(| ?a (~ ?a))",    "true"                      }
+rule! {or_true,     "(| ?a true)",         "true"                      }
+rule! {and_true,    "(& ?a true)",         "?a"                     }
+rule! {contrapositive, "(-> ?a ?b)",    "(-> (~ ?b) (~ ?a))"     }
+rule! {lem_imply, "(& (-> ?a ?b) (-> (~ ?a) ?c))", "(| ?b ?c)"}
+
+fn prove_something(name: &str, start: &str, rewrites: &[Rewrite], goals: &[&str]) {
+    let _ = env_logger::builder().is_test(true).try_init();
+    println!("Proving {}", name);
+
+    let start_expr: RecExpr<_> = start.parse().unwrap();
+    let goal_exprs: Vec<RecExpr<_>> = goals.iter().map(|g| g.parse().unwrap()).collect();
+
+    let egraph = Runner::default()
+        .with_iter_limit(20)
+        .with_node_limit(5_000)
+        .with_expr(&start_expr)
+        .run(rewrites)
+        .egraph;
+
+    for (i, (goal_expr, goal_str)) in goal_exprs.iter().zip(goals).enumerate() {
+        println!("Trying to prove goal {}: {}", i, goal_str);
+        let equivs = egraph.equivs(&start_expr, &goal_expr);
+        if equivs.is_empty() {
+            panic!("Couldn't prove goal {}: {}", i, goal_str);
+        }
+    }
+}
+
+#[test]
+fn prove_contrapositive() {
+    let _ = env_logger::builder().is_test(true).try_init();
+    let rules = &[def_imply(), def_imply_flip(), double_neg_flip(), comm_or()];
+    prove_something(
+        "contrapositive",
+        "(-> x y)",
+        rules,
+        &[
+            "(-> x y)",
+            "(| (~ x) y)",
+            "(| (~ x) (~ (~ y)))",
+            "(| (~ (~ y)) (~ x))",
+            "(-> (~ y) (~ x))",
+        ],
+    );
+}
+
+#[test]
+fn prove_chain() {
+    let _ = env_logger::builder().is_test(true).try_init();
+    let rules = &[
+        // rules needed for contrapositive
+        def_imply(),
+        def_imply_flip(),
+        double_neg_flip(),
+        comm_or(),
+        // and some others
+        comm_and(),
+        lem_imply(),
+    ];
+    prove_something(
+        "chain",
+        "(& (-> x y) (-> y z))",
+        rules,
+        &[
+            "(& (-> x y) (-> y z))",
+            "(& (-> (~ y) (~ x)) (-> y z))",
+            "(& (-> y z)         (-> (~ y) (~ x)))",
+            "(| z (~ x))",
+            "(| (~ x) z)",
+            "(-> x z)",
+        ],
+    );
+}
+
+#[test]
+fn const_fold() {
+    let start = "(| (& false true) (& true false))";
+    let start_expr = start.parse().unwrap();
+    let end = "false";
+    let end_expr = end.parse().unwrap();
+    let mut eg = EGraph::default();
+    eg.add_expr(&start_expr);
+    eg.rebuild();
+    assert!(!eg.equivs(&start_expr, &end_expr).is_empty());
+}
