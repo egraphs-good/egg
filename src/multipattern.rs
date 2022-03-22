f3cfa4e87f074ae57c792f1b74d8da6461e9d847
Max Willsey <me@mwillsey.com>
2022-03-22 10:12:06 -0700

    Clean up; add a couple tests

--- src/multipattern.rs
+++ src/multipattern.rs
@@ -171,14 +171,18 @@ impl<L: Language, A: Analysis<L>> Applier<L, A> for MultiPattern<L> {
     }
 
     fn vars(&self) -> Vec<Var> {
+        let mut bound_vars = HashSet::default();
         let mut vars = vec![];
-        // TODO are unbound binding vars allowed?
-        for (_v, pat) in &self.asts {
+        for (bv, pat) in &self.asts {
             for n in pat.as_ref() {
                 if let ENodeOrVar::Var(v) = n {
-                    vars.push(*v)
+                    // using vars that are already bound doesn't count
+                    if !bound_vars.contains(v) {
+                        vars.push(*v)
+                    }
                 }
             }
+            bound_vars.insert(bv);
         }
         vars.sort();
         vars.dedup();
@@ -193,9 +197,20 @@ mod tests {
     type EGraph = crate::EGraph<S, ()>;
 
     impl EGraph {
-        fn add_string(self: &mut Self, s: &str) -> Id {
+        fn add_string(&mut self, s: &str) -> Id {
             self.add_expr(&s.parse().unwrap())
         }
+    }
+
+    #[test]
+    #[should_panic = "unbound var ?z"]
+    fn bad_unbound_var() {
+        let _: Rewrite<S, ()> = rewrite!("foo"; "?x = (foo ?y)" |- "?x = ?z");
+    }
+
+    #[test]
+    fn ok_unbound_var() {
+        let _: Rewrite<S, ()> = rewrite!("foo"; "?x = (foo ?y)" |- "?z = (baz ?y), ?x = ?z");
     }
 
     #[test]
@@ -254,9 +269,13 @@ mod tests {
         let z1 = egraph.add_string("(tag z ctx2)");
         egraph.union(x1, y1);
         egraph.union(y2, z2);
-        let rules = vec![
-            rewrite!("context-transfer"; "?x = (tag ?a ?ctx1) = (tag ?b ?ctx1), ?t = (lte ?ctx1 ?ctx2), ?a1 = (tag ?a ?ctx2), ?b1 = (tag ?b ?ctx2)" |- "?a1 = ?b1"),
-        ];
+        let rules = vec![rewrite!("context-transfer"; 
+                     "?x = (tag ?a ?ctx1) = (tag ?b ?ctx1), 
+                      ?t = (lte ?ctx1 ?ctx2), 
+                      ?a1 = (tag ?a ?ctx2), 
+                      ?b1 = (tag ?b ?ctx2)" 
+                      |- 
+                      "?a1 = ?b1")];
         let runner = Runner::default().with_egraph(egraph).run(&rules);
         assert_eq!(runner.egraph.find(x1), runner.egraph.find(y1));
         assert_eq!(runner.egraph.find(y2), runner.egraph.find(z2));
@@ -264,7 +283,7 @@ mod tests {
         assert_eq!(runner.egraph.find(x2), runner.egraph.find(y2));
         assert_eq!(runner.egraph.find(x2), runner.egraph.find(z2));
 
-        assert!(runner.egraph.find(y1) != runner.egraph.find(z1));
-        assert!(runner.egraph.find(x1) != runner.egraph.find(z1));
+        assert_ne!(runner.egraph.find(y1), runner.egraph.find(z1));
+        assert_ne!(runner.egraph.find(x1), runner.egraph.find(z1));
     }
 }
